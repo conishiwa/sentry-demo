ca738c7d5782bae84ef76fac61d32187176acb86
Nicolas Ferrand <nicolas.f@vitemonmarche.fr>
2022-03-16 11:57:08 +0100

    add bug

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,9 @@ const viteSentryConfig = {
   org: "nicolas-ferrand",
   project: "vue-test",
   release: `${process.env.npm_package_name}@${process.env.npm_package_version}`,
-  setCommits: {},
+  setCommits: {
+    auto: true,
+  },
   sourceMaps: {
     include: ['./dist/assets'],
     ignore: ['node_modules'],
